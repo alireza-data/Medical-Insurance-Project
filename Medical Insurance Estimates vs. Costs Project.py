1a2bfd6a5475d2f346f10ffffec948fbbfb27b54
Alireza <64637579+alireza-data@users.noreply.github.com>
2022-08-02 10:00:44 +0100

    Add files via upload
    Based on medical and actual insurance estimates, this code gives information on the costs for every single person.

---
+++ Medical Insurance Estimates vs. Costs Project.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+names = ["Judith", "Abel", "Tyson", "Martha", "Beverley", "David", "Anabel"]
+estimated_insurance_costs = [1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 7000.0]
+actual_insurance_costs = [1100.0, 2200.0, 3300.0, 4400.0, 5500.0, 6600.0, 7700.0]
+
+# Add your code here
+total_cost = 0
+for insurance_cost in actual_insurance_costs:
+  total_cost += insurance_cost
+  average_cost = total_cost / len(actual_insurance_costs)
+  
+print("Average Insurance Cost: " + str(average_cost) + "$.")
+for i in range(len(names)):
+  name = names[i]
+  insurance_cost = actual_insurance_costs[i]
+  print("The insurance cost for " + str(name) + " is " + str(insurance_cost) + "dollars.")
+  if insurance_cost > average_cost:
+    print("The insurance cost for " + name +" is above average.")
+  elif insurance_cost < average_cost:
+    print("The insurance cost for " + name +" is below average.")
+  else:
+    print("The insurance cost for " + name +" is equal to the average.")
+
+#Difference between estimated and actual insurance cost
+updated_estimated_costs = [i*11/10 for i in estimated_insurance_costs]
+print(str(updated_estimated_costs))
+
+
+# In[ ]:
+
+
+
+
